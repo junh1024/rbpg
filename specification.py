c46aabd7b3485f8cbf98f8daa4073f4cef408b4c
junh1024 <junh1024@eda70d13-a85d-7d0d-f76b-be8b3802e807>
2014-07-28 05:43:12 +0000

    fix some bugs2

---
+++ specification.py
@@ -0,0 +1,32 @@
+Reaper Batch Project Generator - Specs
+
+Usage: Pythong RBPG.py template.RPP iterations
+
+we assume that there's only 1 number in the template.
+
+Sample lines:
+ NAME "E01 The Girl Who Dashes Off.mkv_ENG.flac"
+ 
+ FILE "E01 The Girl Who Dashes Off.mkv_ENG.flac"
+ 
+   RENDER_FILE "C:\Users\Jun-Hong\Videos\HD2\Kokoro\0009sync.wav"
+   
+Milestones
+   
+   Read file in & spit out a functional copy [DONE]
+   Recognize filenames [DONE]
+   Recognize numbers [DONE]
+   Increment numbers [DONE]
+   Multifile [DONE]
+   Queueing
+   
+More ideas:
+
+Recognize the OP/ED and , make cuts, possibly replace them.
+
+Update just the FXchains given source & DST RPPs
+
+Download pluraleyes?
+
++O Recognize video/pics, replace frames. 
+
