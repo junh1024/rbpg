8c876d00a735f6c456b4c6e53cb93a4954232114
junh1024 <junh1024@eda70d13-a85d-7d0d-f76b-be8b3802e807>
2014-07-28 05:43:12 +0000

    fix some bugs2

---
+++ trunk/RBPG.py
@@ -0,0 +1,49 @@
+# Reaper Batch Project Generator
+# By Jun-Hong, 2014
+
+# http://stackoverflow.com/questions/3141642/increment-a-version-number-using-regular-expression
+
+import re, sys,string
+
+in_file=sys.argv[1]
+iterations=int(sys.argv[2])
+
+linecounter=0
+
+length=0
+modifiedline=""
+
+#recognizing the filename number
+filenamePat = re.compile(r'(.*?)(\d+)(.*)')
+filenamematch = filenamePat.search(in_file)
+
+if filenamematch:
+	filecounter=int(filenamematch.group(2)) #set the initial filecounter number
+	
+read_file = open(in_file).readlines() 
+
+
+# The stuff we want to match
+nameInProjPat = re.compile(r'(\s+NAME.*?|\s+FILE.*?)(\d+)(.*$)')
+
+# Do the IOps
+for file in range(0,iterations):
+
+	write_file = open(filenamematch.group(1)+str(filecounter+1)+filenamematch.group(3), "w") 
+
+	for line in read_file:
+		
+		m1 = nameInProjPat.search(line)
+		# m2 = fileInProjPat.search(line)
+		if m1:
+			incremented= int(m1.group(2)) #convert to int so it can be incremented
+			incremented+=file+1
+			incremented=str(incremented)
+			incremented=incremented.zfill(m1.group(2).__len__()) #pad zeroes
+			line=m1.group(1)+incremented+m1.group(3)+"\n" #reassemble the line
+		
+		write_file.write(line)
+		
+		linecounter+=1
+	write_file.close
+	filecounter+=1
